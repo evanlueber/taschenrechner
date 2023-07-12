5511d69336ae048756a5c3832d503c1979a5a19e
Evan <evan.lueber@stud.kbw.ch>
2023-07-12 09:07:13 +0200

    handle onClick event

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,6 @@
+import { CalcContext } from "../context/CalcContext";
+import React, { useContext } from "react";
+
 const getStyleName = btn => {
   const className = {
     '=': 'equals',
@@ -10,8 +13,110 @@ const getStyleName = btn => {
 }
 
 const Button = ({ value }) => {
+  const { calc, setCalc } = useContext(CalcContext)
+
+  // User click comma
+  const commaClick = () => {
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes('.') ? calc.num + value : calc.num
+    })
+  }
+
+  // User click C
+  const resetClick = () => {
+    setCalc({
+      sign: '' ,
+      num: 0,
+      res: 0
+    })
+  }
+
+  // User click number
+  const handleClickButton = () => { 
+    const numberString = value.toString()
+
+    let numberValue
+    if(numberString === '0' && calc.nu === 0) {
+      numberValue = '0'
+    } else {
+      numberValue = Number(calc.num + numberString)
+    }
+
+    setCalc({
+      ...calc,
+      num: numberValue,
+    })
+  }
+
+  // User click sign
+  const signClick = () => {
+    setCalc({
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0
+    })
+  }
+
+  // User click equals
+  const equalsClick = () => {
+    if(calc.res && calc.num) {
+      const math = (a, b, sign) => {
+        const result = {
+          '+': (a, b) => a + b,
+          '-': (a, b) => a - b,
+          'x': (a, b) => a * b,
+          '/': (a, b) => a / b,
+        }
+        return result[sign](a, b) 
+      }
+      setCalc({
+        res: math(calc.res, calc.num, calc.sign),
+        sign: '',
+        num: 0
+      })
+    }
+  }
+
+  // User click percent 
+  const percentClick = () => {
+    setCalc({
+      num: (calc.num / 100),
+      res: (calc.res / 100),
+      sign: ''
+    })
+  }
+
+  // user click invert
+  const invertClick = () => {
+    setCalc({
+      num: calc.num ? calc.num * -1 : 0,
+      res: calc.res ? calc.res * -1 : 0,
+      sign: ''
+    })    
+  }
+
+  const handleBtnClick  = () => {
+    const results = {
+      '.': commaClick,
+      'C': resetClick,
+      '/': signClick,
+      'x': signClick,
+      '-': signClick,
+      '+': signClick,
+      '=': equalsClick,
+      '%': percentClick,
+      '+-': invertClick
+    }
+    if(results[value]) {
+      return results[value]();
+    } else {
+      return handleClickButton();
+    }
+  }
+
   return (
-    <button className={`${getStyleName(value)} button`}>{value}</button>
+    <button className={`${getStyleName(value)} button`} onClick={handleBtnClick}>{value}</button>
   )
 }
 
--- src/context/CalcContext.jsx
+++ src/context/CalcContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useState } from 'react';
 
 export const CalcContext = createContext();
 const CalcProvider = ({ children }) => {
-    const {calc, setCalc} = useState({
+    const [calc, setCalc] = useState({
         sign: "",
         num: 0,
         res: 0
